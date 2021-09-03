ce382a6fdac552ca668c27bff8b5c9e5757fcc81
Chrissy121 <crys121@gmail.com>
2021-09-02 23:30:53 -0400

    API ready

--- src/test/java/com/bookit/step_definitions/APIStepDefs.java
+++ src/test/java/com/bookit/step_definitions/APIStepDefs.java
@@ -1,13 +1,26 @@
 package com.bookit.step_definitions;
 
 import com.bookit.utilities.BookITUtils;
+import com.bookit.utilities.ConfigurationReader;
+import io.cucumber.datatable.dependency.com.fasterxml.jackson.databind.annotation.JacksonStdImpl;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.defaultParser;
+import static io.restassured.RestAssured.given;
 
 public class APIStepDefs {
     String token;
     String emailGlobal;
     String passwordGlobal;
+    public static String apiName;
+    public static String apiRole;
+    public static String apiTeam;
+    public static String apiBatch;
+    public static String apiCampus;
 
     @When("User logs in BookIT API using {string} and {string}")
     public void user_logs_in_BookIT_API_using_and(String email, String password) {
@@ -18,6 +31,25 @@ public class APIStepDefs {
 
     @Then("User gets API information")
     public void user_gets_API_information() {
+
+        Response response = given().accept(ContentType.JSON)
+                .and().header("Authorization" , token)
+                .when()
+                .get(ConfigurationReader.get("apiUrl")+"/api/students/me");
+
+        System.out.println(response.statusCode());
+
+        JsonPath jsonPath = response.jsonPath();
+        apiName = jsonPath.getString("firstName") + " " + jsonPath.getString("lastName");
+        apiRole = jsonPath.getString("role");
+
+        String[] otherQueries =  BookITUtils.getMYInfo(emailGlobal, passwordGlobal);
+        apiTeam = otherQueries[0];
+        apiBatch = otherQueries[1];
+        apiCampus = otherQueries[2];
+        System.out.println(apiCampus);
+
+
 
     }
 
--- src/test/java/com/bookit/utilities/BookITUtils.java
+++ src/test/java/com/bookit/utilities/BookITUtils.java
@@ -1,6 +1,7 @@
 package com.bookit.utilities;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
@@ -25,18 +26,39 @@ public class BookITUtils {
     }
 
     public static String[] getMYInfo(String email, String password){
-        String[] myInfo = new String[];
+        String[] myInfo = new String[3];
 
         for (int i = 0; i < 3; i++) {
             if (i==0){
-                //get batch request
-                //myInfo[i]=jsonPath.getString
+                Response response = given().accept(ContentType.JSON)
+                        .and().header("Authorization", generateToken(email, password))
+                        .when()
+                        .get(ConfigurationReader.get("apiUrl")+"/api/teams/my");
+
+                JsonPath jsonPath = response.jsonPath();
+                myInfo[i]= jsonPath.getString("name");
             }
             if(i==1){
                 //get team request
+                Response response = given().accept(ContentType.JSON)
+                        .and().header("Authorization", generateToken(email, password))
+                        .when()
+                        .get(ConfigurationReader.get("apiUrl")+"/api/batches/my");
+
+                JsonPath jsonPath = response.jsonPath();
+                myInfo[i]= jsonPath.getString("number");
             }
-            
+            if(i==2){
+                //get campus request
+                Response response = given().accept(ContentType.JSON)
+                        .and().header("Authorization", generateToken(email, password))
+                        .when()
+                        .get(ConfigurationReader.get("apiUrl")+"/api/campuses/my");
+
+                JsonPath jsonPath = response.jsonPath();
+                myInfo[i]= jsonPath.getString("location");
+            }
         }
-        
+        return myInfo;
     }
 }
